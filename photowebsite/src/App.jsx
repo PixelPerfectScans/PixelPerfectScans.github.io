a10b25e81a5e640fa2448b50b7fb0c3a084762c9
Brandon Tippings <tippingsbrandon@gmail.com>
2025-08-26 15:49:15 -0700

    Added example images

--- photowebsite/src/App.jsx
+++ photowebsite/src/App.jsx
@@ -11,6 +11,19 @@ import { GrUpgrade } from "react-icons/gr";
 import { FaShieldAlt } from "react-icons/fa";
 import { HiOutlineEmojiHappy } from "react-icons/hi";
 import { LuFolderTree } from "react-icons/lu";
+
+import original from "../public/result/album3_0089.jpg";
+import enhanced from "../public/result/album3_0089_a.jpg";
+import back from "../public/result/album3_0089_b.jpg";
+
+import kittyOriginal from "../public/enhanced/4x6b_0159.jpg";
+import kittyEnhanced from "../public/enhanced/4x6b_0159_a.jpg";
+
+import elmoOriginal from "../public/enhanced/4x6b_0007.jpg";
+import elmoEnhanced from "../public/enhanced/4x6b_0007_a.jpg";
+
+import maddieOriginal from "../public/enhanced/4x6b_0168.jpg";
+import maddieEnhanced from "../public/enhanced/4x6b_0168_a.jpg";
 // Hero Section
 const Hero = () => (
     <header className="hero">
@@ -23,47 +36,14 @@ const Hero = () => (
         <button className="demo-btn">Get a Quote</button>
         </nav>
         <div className="hero-content">
-            <h1>PixelPerfectMemories</h1>
+            <h1>PixelPerfectScans</h1>
             <p>Turning your physical photos into timeless digital copies.</p>
         </div>
     </header>
 );
 
-// Stats Section
-const Stats = () => (
-    <section className="stats">
-        <div className="stat-item">
-            <h2>1.4M+</h2>
-            <p>homeowners</p>
-        </div>
-        <div className="stat-item">
-            <h2>11%</h2>
-            <p>high‑intent leads generated</p>
-        </div>
-        <div className="stat-item">
-            <h2>11+</h2>
-            <p>offer types</p>
-        </div>
-    </section>
-);
 
-// Audience Cards
-const AudienceCards = () => (
-    <section className="audience-cards">
-        {[
-            { title: "Safe", description: "No need to ship your photos. I can personally transport your photos.", image: <AiFillSafetyCertificate /> },
-            { title: "Private" , description: "No cloud copies are made unless specifically requested.", image:<RiGitRepositoryPrivateLine /> },
-            { title: "Affordable", description: "Digitize photos for mere pennies each. Bulk discounts offered.",image:<MdAttachMoney /> },
-            { title: "Quick", description: "Most collections are copied and returned with 48 hours.", image:<IoSpeedometerOutline /> }
-        ].map(({ title, description,image }) => (
-            <div key={title}  className="card">
-                <h3>{title}   {image}</h3>
 
-                <p>{description}</p>
-            </div>
-        ))}
-    </section>
-);
 
 // Features Section
 const Features = () => (
@@ -71,7 +51,7 @@ const Features = () => (
         <div className="feature">
             <h3>Share   <FaRegShareSquare />
             </h3>
-            <p>Your digital copies are just an email away from sharing with family and friends</p>
+            <p>Your digital photos are just an email away from sharing with family and friends</p>
         </div>
         <div className="feature">
             <h3>Enhance <GrUpgrade/></h3>
@@ -92,32 +72,109 @@ const Features = () => (
     </section>
 );
 
-// Mission Statement
+
+
+const AudienceCards = () => (
+    <section className="audience-cards">
+        {[
+            { title: "Safe", description: "No need to ship your photos. I can personally transport your photos.", image: <AiFillSafetyCertificate /> },
+            { title: "Private" , description: "No cloud copies are made unless specifically requested.", image:<RiGitRepositoryPrivateLine /> },
+            { title: "Affordable", description: "Digitize photos for just pennies each. Bulk pricing offered.",image:<MdAttachMoney /> },
+            { title: "Efficient", description: "Most collections are copied and returned with 48 hours.", image:<IoSpeedometerOutline /> }
+        ].map(({ title, description,image }) => (
+            <div key={title}  className="card">
+                <h3>{title}   {image}</h3>
+
+                <p>{description}</p>
+            </div>
+        ))}
+    </section>
+);
+
+
 const Mission = () => (
     <section className="mission">
         <h2>Preserve what matters most</h2>
     </section>
 );
 
-// Team & Testimonials
+
 const TeamAndTestimonials = () => (
     <section className="team-testimonials">
 
         <div className="testimonials">
-            <h3>Hear from our customers</h3>
-            {/* Add testimonial content */}
+            <h3>Hear from our customers:</h3>
+            <section className="audience-cards">
+                {[
+                    { title: "Jane Doe", description: "\"I am so glad I found PixelPerfectScans and had my photo collection digitized. I think the biggest perk was being able to share with my family. I adore my digital photo frame where I can see new pictures each day.\""},
+                    { title: "John Doe" , description: "\"B is redefining what the human experience means.\""},
+                    { title: "Jason Doe" , description: "\"The greatest thing since sliced bread.\""},
+                    { title: "JJ Doe" , description: "\"I named my firstborn after B because they changed my life.\""},
+                ].map(({ title, description }) => (
+                    <div key={title}  className="card">
+                        <h3>{title} </h3>
+
+                        <p>{description}</p>
+                    </div>
+                ))}
+            </section>
         </div>
     </section>
 );
 
-// Integration CTA
-const IntegrationCTA = () => (
-    <section className="integration">
-        <h3>PixelPerfectMemories helps you save and enjoy </h3>
-        <p>Whether you service in‑house or use a subservicer, launch deeper engagement with your borrowers in weeks (not months)...</p>
-        <button className="demo-btn">Get a Demo</button>
-    </section>
-);
+const ExamplePhotos = () => (
+    <>
+        {/*<h3>Example Results</h3>*/}
+        <section className="example-photos">
+            <div className="example-photos-row">
+                <div className="example-photos-card">
+                    <h2>Original</h2>
+                    <img className="examplephoto" src={original} alt={"original"} />
+                </div>
+                <div className="example-photos-card">
+                    <h2>Enhanced</h2>
+                    <img className="examplephoto" src={enhanced} alt={"enhanced"}/>
+                </div>
+                <div className="example-photos-card">
+                    <h2>Back</h2>
+                    <img className="examplephoto" src={back} alt={"back"}/>
+                </div>
+            </div>
+            <div className="example-photos-row">
+                <div className="example-photos-card">
+                    <h2>Original</h2>
+                    <img className="examplephoto" src={maddieOriginal} alt={"original"} />
+                </div>
+                <div className="example-photos-card">
+                    <h2>Enhanced</h2>
+                    <img className="examplephoto" src={maddieEnhanced} alt={"enhanced"}/>
+                </div>
+            </div>
+            <div className="example-photos-row">
+                <div className="example-photos-card">
+                    <h2>Original</h2>
+                    <img className="examplephoto" src={elmoOriginal} alt={"original"} />
+                </div>
+                <div className="example-photos-card">
+                    <h2>Enhanced</h2>
+                    <img className="examplephoto" src={elmoEnhanced} alt={"enhanced"}/>
+                </div>
+            </div>
+            <div className="example-photos-row">
+                <div className="example-photos-card">
+                    <h2>Original</h2>
+                    <img className="examplephoto" src={kittyOriginal} alt={"original"} />
+                </div>
+                <div className="example-photos-card">
+                    <h2>Enhanced</h2>
+                    <img className="examplephoto" src={kittyEnhanced} alt={"enhanced"}/>
+                </div>
+            </div>
+        </section>
+    </>
+)
+
+
 
 // Footer
 const Footer = () => (
@@ -154,17 +211,18 @@ const Footer = () => (
     </footer>
 );
 
-// Main App Component
 const App = () => (
-    <div>
+    <div >
         <Hero />
         {/*<Stats />*/}
         <Features />
-        <AudienceCards />
 
         <Mission />
+        <ExamplePhotos />
+
+        <AudienceCards />
+
         <TeamAndTestimonials />
-        <IntegrationCTA />
         <Footer />
     </div>
 
